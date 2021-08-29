9b11b5d1ebee3042e0425d4529595d3fe29df921
Kaan Koç <kock@mef.edu.tr>
2021-08-29 17:47:20 +0200

    Initial Commit

---
+++ code-tracker/code_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_trackerApp.swift
+//  code-tracker
+//
+//  Created by Kaan Koç on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct code_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
